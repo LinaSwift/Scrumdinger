114f9e834ce3cec27c46acaa3867a2ca43680369
LinaK <gaklinskikh@gmail.com>
2024-02-06 16:33:41 +0500

    passing data with bindings

---
+++ Scrumdinger/Views/ThemePicker.swift
@@ -0,0 +1,8 @@
+//
+//  ThemePicker.swift
+//  Scrumdinger
+//
+//  Created by Galina Klinskikh on 06.02.2024.
+//
+
+import Foundation
---
+++ Scrumdinger/Views/ThemeView.swift
@@ -0,0 +1,8 @@
+//
+//  ThemeView.swift
+//  Scrumdinger
+//
+//  Created by Galina Klinskikh on 06.02.2024.
+//
+
+import Foundation
